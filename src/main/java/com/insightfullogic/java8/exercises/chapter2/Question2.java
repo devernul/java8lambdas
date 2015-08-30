679e5367131730e43788fbffca188c142c717c37
Mattias Borgkvist <nightzero@gmail.com>
2015-08-30 20:54:32 +0200

    first commit

---
+++ src/main/java/com/insightfullogic/java8/exercises/chapter2/Question2.java
@@ -0,0 +1,10 @@
+package com.insightfullogic.java8.exercises.chapter2;
+
+import java.text.SimpleDateFormat;
+
+public class Question2 {
+
+	public static ThreadLocal<SimpleDateFormat> formatter = ThreadLocal
+			.withInitial(() -> new SimpleDateFormat("dd-MMM-yyyy"));
+
+}
