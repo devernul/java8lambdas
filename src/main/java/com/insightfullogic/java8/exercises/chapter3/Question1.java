602d21d91655f7b664b06c38aac7a86b4f519824
Mattias Borgkvist <mattias.borgkvist@ericsson.com>
2015-09-04 16:43:04 +0200

    Updated.

--- src/main/java/com/insightfullogic/java8/exercises/chapter3/Question1.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter3/Question1.java
@@ -4,6 +4,7 @@ import com.insightfullogic.java8.examples.chapter1.Album;
 import com.insightfullogic.java8.examples.chapter1.Artist;
 import com.insightfullogic.java8.exercises.Exercises;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -11,14 +12,22 @@ import static java.util.stream.Collectors.toList;
 
 public class Question1 {
     public static int addUp(Stream<Integer> numbers) {
-        return Exercises.replaceThisWithSolution();
+        return numbers.reduce(0, (acc, elem) -> acc+elem);
     }
 
     public static List<String> getNamesAndOrigins(List<Artist> artists) {
-        return Exercises.replaceThisWithSolution();
+    	//return artists.stream().map(a -> a.getName() + ":" + a.getNationality()).collect(toList());
+    	return artists.stream().flatMap(a -> Stream.of(a.getName(), a.getNationality())).collect(toList());
     }
 
     public static List<Album> getAlbumsWithAtMostThreeTracks(List<Album> input) {
         return Exercises.replaceThisWithSolution();
+    }
+    
+    public static void main(String[] args){
+    	List<String> list = Question1.getNamesAndOrigins(Arrays.asList(new Artist("A", "B"), new Artist("C", "D"), new Artist("E", "F")));
+    	for(String s : list){
+    		System.out.println(s);
+    	}
     }
 }
