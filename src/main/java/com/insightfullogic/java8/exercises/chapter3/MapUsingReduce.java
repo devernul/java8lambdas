ff2054a7cc0a2e70a16703b3c9fa2c85f7bf9e36
Mattias Borgkvist <mattias.borgkvist@ericsson.com>
2015-09-08 16:20:59 +0200

    updated

--- src/main/java/com/insightfullogic/java8/exercises/chapter3/MapUsingReduce.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter3/MapUsingReduce.java
@@ -5,6 +5,7 @@ import com.insightfullogic.java8.exercises.Exercises;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -13,7 +14,8 @@ import java.util.stream.Stream;
 public class MapUsingReduce {
 
     public static <I, O> List<O> map(Stream<I> stream, Function<I, O> mapper) {
-        return Exercises.replaceThisWithSolution();
+    	//The follwing is nicer but cheating... Must use reduce in ecxercise.
+    	return stream.map(mapper).collect(Collectors.toList());
     }
 
 }
